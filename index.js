56c8c38da67229152c35f34cda1cac81ea86d513
Sergey <seregaland@yandex.ru>
2022-10-26 16:24:36 +0300

    init

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
 
-const token = process.env.TELEGRAM_API_TOKEN;
+const token = '5360466924:AAHNPVR7bV9269oe-pzxtzTuj58HIR4Jf5c';
 const webAppUrl = 'https://inspiring-manatee-7e59f8.netlify.app';
 
 const bot = new TelegramBot(token, {polling: true});
