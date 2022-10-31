2fca598ec8d489ca6a50c1708a4ed0fc5d50f533
Sergey <seregaland@yandex.ru>
2022-10-31 14:43:12 +0300

    update

--- index.js
+++ index.js
@@ -65,13 +65,6 @@ bot.on('message', async (msg) => {
             ]
         }
     })
-
-    /*await bot.on('message', (msg) => {
-          const chatId = msg.chat.id;
-
-          // send a message to the chat acknowledging receipt of their message
-          bot.sendMessage(chatId, 'Получил ваше сообщение');
-    });*/
   }
 
   if (text === '/notion') {
