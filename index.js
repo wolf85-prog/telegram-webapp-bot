bbc5de2442e52ffbff44abed642f608eb3873cd7
seregey gornih <you@example.com>
2022-11-23 23:06:00 +0300

    test init

--- index.js
+++ index.js
@@ -225,39 +225,44 @@ bot.on('callback_query', msg => {
     bot.sendMessage(chatId, `Вы нажали кнопку ${data}`, backOptions)
 });
 
-app.post('/web-data', async (req, res) => {
-    const {queryId, products = [], totalPrice} = req.body;
-    try {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Успешная покупка',
-            input_message_content: {
-                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
-            }
-        })
-        return res.status(200).json({});
-    } catch (e) {
-        return res.status(500).json({})
-    }
-})
-
 // app.post('/web-data', async (req, res) => {
-//   const {queryId, products} = req.body;
-//   try {
-//       await bot.answerWebAppQuery(queryId, {
-//           type: 'article',
-//           id: queryId,
-//           title: 'Проект успешно создан',
-//           input_message_content: {
-//               message_text: ` Название проекта:  ${products.map(item => item.title).join(', ')}`
-//           }
-//       })
-//       return res.status(200).json({});
-//   } catch (e) {
-//       return res.status(500).json({})
-//   }
+//     const {queryId, products = [], totalPrice} = req.body;
+//     try {
+//         await bot.answerWebAppQuery(queryId, {
+//             type: 'article',
+//             id: queryId,
+//             title: 'Успешная покупка',
+//             input_message_content: {
+//                 message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+//             }
+//         })
+//         return res.status(200).json({});
+//     } catch (e) {
+//         return res.status(500).json({})
+//     }
 // })
+
+app.get('/secret',(req, res) => {
+    const secret =  Math.floor(Math.random()*100)
+    res.json({secret})
+});
+
+app.post('/web-data', async (req, res) => {
+  const {queryId, projectname} = req.body;
+  try {
+      await bot.answerWebAppQuery(queryId, {
+          type: 'article',
+          id: queryId,
+          title: 'Проект успешно создан',
+          input_message_content: {
+              message_text: ` Название проекта:  ${projectname}`
+          }
+      })
+      return res.status(200).json({});
+  } catch (e) {
+      return res.status(500).json({})
+  }
+})
 
 const PORT = 8000;
 
