cbaa1293aaf973b315f3a4f72ce1457be2d26cbb
seregey gornih <you@example.com>
2022-11-28 08:36:44 +0300

    fix date

--- index.js
+++ index.js
@@ -144,8 +144,8 @@ async function getDatabase() {
                id: page.id,
                title: page.properties.Name.title[0]?.plain_text,
                time: page.properties.Date.date,
-               geo: page.properties.Адрес.rich_text,
-               teh: page.properties.Тех. Задание.rich_text,
+               geo: '',//page.properties.Адрес.rich_text,
+               teh: '',//page.properties.Тех. Задание.rich_text,
                status_id: page.properties.Status.select,
             };
         });
