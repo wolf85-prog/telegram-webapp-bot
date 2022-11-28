bc54213dd1bf1c4c8a959c3528faf97d7e733b55
seregey gornih <you@example.com>
2022-11-28 07:50:08 +0300

    get date

--- index.js
+++ index.js
@@ -130,10 +130,10 @@ async function getDatabase() {
                // Manager: page.properties.Manager.id,
                id: page.id,
                title: page.properties.Name.title[0]?.plain_text,
-               time: '',
+               time: page.properties.Date.date,
                geo: '',
                teh: '',
-               status: Math.floor(Math.random()*9),//page.properties.Status.id,
+               status: '',
             };
         });
 
