d2a0d80d01274debb8c226e3dd59b9bbe31c254f
Sergey <seregaland@yandex.ru>
2022-12-09 10:00:57 +0300

    fix

--- index.js
+++ index.js
@@ -211,7 +211,16 @@ async function getProjectsId(managerId) {
     try {
         const response = await notion.databases.query({
             database_id: databaseId,
-            
+            "filter": {
+                // "property": "Manager",
+                // "relation": [{
+                //     "id": managerId
+                // }]
+                "property": "City",
+                "select": {
+                    "name": "Казань"
+                }
+            }
         });
 
         //return response.results[0].id;
@@ -229,7 +238,7 @@ async function getProjectsId(managerId) {
         //     };
         // });
 
-        return response;
+        return response.results;
     } catch (error) {
         console.error(error.body)
     }
