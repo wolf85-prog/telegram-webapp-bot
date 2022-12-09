fc225539067e9608f7503a44bbfc7eb8ae34dd66
Sergey <seregaland@yandex.ru>
2022-12-09 10:23:45 +0300

    fix

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ async function getProjectsId(managerId) {
                 // }]
                 "property": "City",
                 "select": {
-                    "name": "Москва"
+                    "name": managerId
                 }
             }
         });
