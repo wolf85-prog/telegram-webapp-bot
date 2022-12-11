cffc64c8ebeaad19397576864fa41148f5fb4590
seregey gornih <you@example.com>
2022-12-11 12:09:54 +0300

    fix

--- index.js
+++ index.js
@@ -146,34 +146,34 @@ async function addProject(title, time, teh, managerId) {
                         }
                     ]
                 },
-                children:[
-                    {
-                        "heading_2": {
-                          "rich_text": [
-                            {
-                              "text": {
-                                "content": "Lacinato kale"
-                              }
-                            }
-                          ]
+            },
+            children:[
+                {
+                    "heading_2": {
+                      "rich_text": [
+                        {
+                          "text": {
+                            "content": "Lacinato kale"
+                          }
                         }
-                      },
-                      {
-                        "paragraph": {
-                          "rich_text": [
-                            {
-                              "text": {
-                                "content": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
-                                "link": {
-                                  "url": "https://en.wikipedia.org/wiki/Lacinato_kale"
-                                }
-                              }
+                      ]
+                    }
+                  },
+                  {
+                    "paragraph": {
+                      "rich_text": [
+                        {
+                          "text": {
+                            "content": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
+                            "link": {
+                              "url": "https://en.wikipedia.org/wiki/Lacinato_kale"
                             }
-                          ]
+                          }
                         }
-                      }
-                ]
-            }
+                      ]
+                    }
+                  }
+            ]
         })
         console.log(response)
         console.log("Success! Entry added.")
