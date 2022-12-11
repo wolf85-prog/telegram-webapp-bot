f9a0191f5f95e74a0f6352aceff7b8e8f9a6bec7
seregey gornih <you@example.com>
2022-12-11 21:30:55 +0300

    fix

--- index.js
+++ index.js
@@ -550,7 +550,7 @@ async function getCompanyId(id) {
             }
         });
 
-        return response.results[0].Должность.id;
+        return response.results[0];
     } catch (error) {
         console.error(error.body)
     }
