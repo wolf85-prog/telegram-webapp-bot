3c1d99dba79cfa0626300829401000750dccf1c7
seregey gornih <you@example.com>
2022-12-14 22:59:53 +0300

    fix

--- index.js
+++ index.js
@@ -71,10 +71,10 @@ bot.setMyCommands([
 bot.on('message', msg => {
     const text = msg.text;
     const chat_id = msg.chat.id;
-    const chat_admin_id = '1698411118';
+    const chat_admin_id = '-800573397';
     if (!text.includes('/')) {       
         if (text.includes("Ответ")) {           
-            bot.sendMessage(text.substring(6), `Ваше сообщение получено администратором!`)
+            bot.sendMessage(text.substring(6, text.indexOf('.')), text.substring(text.indexOf('Текст:')))
         } else {
             bot.sendMessage(chat_id, `Ваше сообщение "${text}" отправлено администратору!`)
             bot.sendMessage(chat_admin_id, `Новое сообщение "${text}" от ${msg.from.first_name} ${msg.from.last_name} ${chat_id}`)           
