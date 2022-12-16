49ed5986be4b268c0104f4ced7983da805a4a7ac
seregey gornih <you@example.com>
2022-12-16 23:44:44 +0300

    send message

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ bot.on('message', msg => {
             bot.sendMessage(text.substring(6, text.indexOf('.')), text.substring(text.indexOf('Текст:')))
         } else if (text.includes('Проект успешно создан')) {
             bot.sendMessage(chat_id, 'Ваша заявка отправлена администратору!')
+            bot.sendMessage(chat_admin_id, `${text} \n \n от ${msg.from.first_name} ${msg.from.last_name} ${chat_id}`)
         } else if (text.includes('Всю информацию вы получите в этом чате')) {
 
         } else {
