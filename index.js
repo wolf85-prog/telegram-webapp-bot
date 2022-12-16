f47a86e86c7f76964195c14fa840a56825164b85
Sergey <seregaland@yandex.ru>
2022-12-16 18:03:07 +0300

    fix

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ async function addProject(title, time, teh, managerId, companyId, worklist, geoI
                     date: {
                         "start": time,
                         "end": null,
-                        "time_zone": null
+                        "time_zone": "Europe/Moscow"
                     }
                 },
                 TechZadanie: {
@@ -593,7 +593,7 @@ async function addDate(blockId, day) {
                 Date : {
                     type: 'date',                   
                     date: {
-                        "start": "2022-01-01T00:00",
+                        "start": "2022-01-01T00:00:00+03:00",
                         "end": null,
                         "time_zone": null
                     }
@@ -618,7 +618,7 @@ async function addWorker(blockId, worker) {
                 "1. Дата": {
                     type: "date",
                     date: {
-                        "start": "2022-10-30T00:00",
+                        "start": "2022-10-30T00:00:00+03:00",
                         "end": null,
                         "time_zone": null
                     }
@@ -660,7 +660,7 @@ async function addWorkerZapas(blockId) {
                 "1. Дата": {
                     "type": "date",
                     date: {
-                        "start": "2022-10-30T00:00",
+                        "start": "2022-10-30T00:00:00+03:00",
                         "end": null,
                         "time_zone": null
                     }
