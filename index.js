3c557110dfc354f648d5bb5391a237206196f71c
seregey gornih <you@example.com>
2022-12-21 20:42:09 +0300

    fix blocks

--- index.js
+++ index.js
@@ -870,8 +870,11 @@ async function getBlocks(blockId) {
         });
 
         let res;
-
-        (count >1) ? res = response.results[1].id : res = response.results[0].id
+        if (response) {
+            (count >1) ? res = response.results[1].id : res = response.results[0].id
+        } else {
+            res = '' 
+        }      
         
         console.log("Blocks Data: "  + JSON.stringify(res))
 
