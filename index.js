9761f24585d56bad8e4563b0b6af15ba04adbb3b
seregey gornih <you@example.com>
2023-01-05 20:15:38 +0300

    fix

--- index.js
+++ index.js
@@ -1487,7 +1487,7 @@ bot.on('message', async (msg) => {
     }
 
     if (text === '/cron') {
-        cron.schedule('1-3 * * * *', () => {
+        cron.schedule('1 * * * *', () => {
             bot.sendMessage(chatId, 
                 'текст сообщения'                                                
             )
