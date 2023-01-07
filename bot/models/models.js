949b7746d0810417b22732f54db69bf9c4264fed
seregey gornih <you@example.com>
2023-01-07 18:51:06 +0300

    fix models

--- bot/models/models.js
+++ bot/models/models.js
@@ -1,20 +1,12 @@
-const {sequelize} = require('../connections/db')
+const sequelize = require('../connections/db')
 const {DataTypes} = require('sequelize')
 
 const User = sequelize.define('user', {
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        unique: true,
-        allowNull: false,    
-    },
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     username: {type: DataTypes.STRING, allowNull: false},
     email: {type: DataTypes.STRING, unique: true},
     password: {type: DataTypes.STRING},
     role: {type: DataTypes.STRING, defaultValue: "USER"},
 })
 
-module.exports = {
-    User
-}
+module.exports = {User}
