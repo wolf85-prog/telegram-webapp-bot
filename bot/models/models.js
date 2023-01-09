583ff570869f82e4081a5dfdbd54bb20103f8bcf
Sergey <seregaland@yandex.ru>
2023-01-09 15:37:21 +0300

    fix model username

--- bot/models/models.js
+++ bot/models/models.js
@@ -3,7 +3,7 @@ const {DataTypes} = require('sequelize')
 
 const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    username: {type: DataTypes.STRING, allowNull: false},
+    username: {type: DataTypes.STRING, allowNull: true},
     email: {type: DataTypes.STRING, unique: true},
     password: {type: DataTypes.STRING},
     role: {type: DataTypes.STRING, defaultValue: "USER"},
