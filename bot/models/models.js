f0aae6e72b5b492c577b64163d04b120944ebaed
Sergey <seregaland@yandex.ru>
2023-01-13 16:21:42 +0300

    fix create user

--- bot/models/models.js
+++ bot/models/models.js
@@ -11,8 +11,8 @@ const User = sequelize.define('user', {
 
 const UserBot = sequelize.define('userbot', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    firstname: {type: DataTypes.STRING},
-    lastname: {type: DataTypes.STRING},
+    first_name: {type: DataTypes.STRING},
+    last_name: {type: DataTypes.STRING},
     chatId: {type: DataTypes.STRING, unique: true},
 })
 
