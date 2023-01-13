bdd2d277f515f6fe43e082a2604471d9d742638f
Sergey <seregaland@yandex.ru>
2023-01-13 11:32:11 +0300

    fix route

--- bot/controllers/projectController.js
+++ bot/controllers/projectController.js
@@ -2,34 +2,39 @@ const { Client } = require("@notionhq/client");
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_DATABASE_ID
 
+async function getProjects() {
+    try {
+        const response = await notion.databases.query({
+            database_id: databaseId
+        });
+
+        const responseResults = response.results.map((page) => {
+            return {
+               id: page.id,
+               title: page.properties.Name.title[0]?.plain_text,
+               time: page.properties.Date.date,
+               time_start: page.properties.Date.date.start,
+               time_created: page.created_time,
+               geo: '',//page.properties.Address.rollup.array,
+               teh: page.properties.TechZadanie.rich_text,
+               status_id: page.properties.Status.select,
+               manager: page.properties.Manager.relation[0]?.id,
+               company: page.properties.Company.relation[0]?.id,
+               worklist:'',
+            };
+        });
+
+        return responseResults;
+    } catch (error) {
+        console.error(error.body)
+    }
+}
+
 class ProjectController {
     
-    async getProjects(req, res) {
-        try {
-            const response = await notion.databases.query({
-                database_id: databaseId
-            });
-    
-            const responseResults = response.results.map((page) => {
-                return {
-                   id: page.id,
-                   title: page.properties.Name.title[0]?.plain_text,
-                   time: page.properties.Date.date,
-                   time_start: page.properties.Date.date.start,
-                   time_created: page.created_time,
-                   geo: '', //page.properties.Address.rollup.array,
-                   teh: page.properties.TechZadanie.rich_text,
-                   status_id: page.properties.Status.select,
-                   manager: page.properties.Manager.relation[0]?.id,
-                   company: page.properties.Company.relation[0]?.id,
-                   worklist:'',
-                };
-            });
-    
-            return responseResults;
-        } catch (error) {
-            console.error(error.body)
-        }
+    async projects(req, res) {
+        const projects = await getProjects();
+        res.json(projects);
     }
 
     async getProjects2(req, res) {
--- bot/routes/index.js
+++ bot/routes/index.js
@@ -8,7 +8,7 @@ const projectController = require('../controllers/projectController')
 router.get('/secret', secretController.secretInfo)
 
 // get PROJECTS  
-router.get("/projects3", projectController.getProjects);
+router.get("/projects3", projectController.projects);
 
 router.get("/projects4", projectController.getProjects2);
 
