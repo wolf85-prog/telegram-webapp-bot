3161ebdf3462960a77028980c47a043f92cb9926
Sergey <seregaland@yandex.ru>
2023-02-02 09:44:53 +0300

    fix set conversation

--- bot.js
+++ bot.js
@@ -1348,31 +1348,31 @@ bot.on('message', async (msg) => {
                         } else {
                             console.log('Ошибка работы БД. Пользователь уже существует')
                         } 
+
+                        //2. найти беседу
+                        const conversation = await Conversation.findAll({
+                            where: {
+                                members: {
+                                    [Op.contains]: [item.tgID]
+                                }
+                            },
+                        })             
+
+                        //3. если нет беседы, то создать 
+                        if (conversation.length === 0) {
+                            const conv = await Conversation.create(
+                            {
+                                members: [item.tgID, chatTelegramId],
+                            })
+                            console.log("conversationId: ", conv.id)
+                            conversation_id = conv.id
+                        } else {
+                            console.log('Беседа уже создана в БД')  
+                            console.log("conversationId: ", conversation[0].id)  
+                            conversation_id = conversation[0].id
+                        }
                     }                  
-
-                    //2. найти беседу
-                    const conversation = await Conversation.findAll({
-                        where: {
-                            members: {
-                                [Op.contains]: [item.tgID]
-                            }
-                        },
-                    })             
-
-                    //3. если нет беседы, то создать 
-                    if (conversation.length === 0) {
-                        const conv = await Conversation.create(
-                        {
-                            members: [item.tgID, chatTelegramId],
-                        })
-                        console.log("conversationId: ", conv.id)
-                        conversation_id = conv.id
-                    } else {
-                        console.log('Беседа уже создана в БД')  
-                        console.log("conversationId: ", conversation[0].id)  
-                        conversation_id = conversation[0].id
-                    }
-                    
+                 
                 } catch (error) {
                     console.log(error);
                 }
