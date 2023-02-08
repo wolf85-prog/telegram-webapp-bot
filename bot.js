3fd18b29e74b354b575a3af6b2eedea9171d4150
Sergey <seregaland@yandex.ru>
2023-02-08 14:45:19 +0300

    add report for test

--- bot.js
+++ bot.js
@@ -1011,28 +1011,125 @@ bot.on('message', async (msg) => {
                 await bot.sendMessage(text.substring(6, text.indexOf('.')), text.slice(text.indexOf('.') + 2)) 
             
             } else if (text.includes('Тестовый добавлен')) {
-                //создание проекта в БД
-                const res = await Project.create({ name: projectName, datestart: dateStart, teh: Teh, geo: Geo, managerId: manager_id, companyId: company_id})
-                console.log('Проект успешно добавлен в БД', JSON.stringify(res))   
-                
-                project_id = res.id
+                try {
+                    //создание проекта в БД
+                    const res = await Project.create({ 
+                        name: projectName, 
+                        datestart: dateStart, 
+                        teh: Teh, 
+                        geo: Geo, 
+                        managerId: manager_id, 
+                        companyId: company_id, 
+                        chatId: chatId
+                    })
+                    console.log('Проект успешно добавлен в БД! Project Id: ', res.id)  
+                    
+                    
+                    //получить информацию о проекте (8 секунд)
+                    setTimeout(async () => {
+                        projectId = '34ae9c1c-3443-47c8-b3c8-ecccf07e5aea'
+                        console.log("projectId: ", projectId)
+                        if (projectId !== 'undefined') {
+                            blockId = await getBlocks(projectId);
+                            console.log("blockId: ", blockId)
+                        } else {
+                            console.log("Проект не добавлен в БД!")
+                        }
+                        
+                    }, 2000)
 
-                // try {
-                //     //добавляем chatId в БД
-                //     const proj = await Project.update({
-                //         chatId: chatId
-                //         }, {
-                //         where: {
-                //             id: project_id
-                //         }
-                //     })
-                //     console.log("ChatId успешно сохранен в бд: ", JSON.stringify(proj)) 
-                // } catch (error) {
-                //     console.log("Ошибка сохранения chat_id  в бд: ", error) 
-                // }
-                console.log('Тестовый проект добавлен: ', project_id)
-                console.log('ChatId: ', chatId)
-            
+                    // отправить сообщение пользователю через 30 секунд
+                    setTimeout(() => {bot.sendMessage(chatId, 'Ваша тестовая заявка принята!')}, 8000) // 30 секунд
+
+                    let count_fio;
+                    let count_title;
+                    let count_title2;
+                    const arr_cat = ['Sound', 'Light', 'Video', 'Riggers', 'Stagehands', 'StageGround', 'Trucks', 'Production']
+                    const arr_cat2 = ['Sound', 'Light', 'Video', 'Riggers', 'StageGround', 'Trucks', 'Production']
+                    let i = 0;
+                    let arr_count = [] 
+                    let arr_all = [] 
+
+                    // const workerlist = Worklist.map((item) => ({
+                    //     spec: item.spec
+                    // }))
+
+                
+                    // повторить с интервалом 1 минуту
+                    let timerId = setInterval(async() => {
+                        
+                        let databaseBlock = await getDatabaseId(blockId); 
+                        //console.log("databaseBlock: ", JSON.stringify(databaseBlock))
+
+                        arr_count = [] 
+                        
+                        arr_cat.map((arritem) => {
+                            count_fio = 0;
+                            count_title = 0;
+                            if (databaseBlock) {
+                                databaseBlock.map((value) => {
+                                    if (arritem === value.title) {
+                                        if (value.fio) {
+                                            count_fio++               
+                                        }else {
+                                            count_fio;
+                                        }  
+                                        count_title++;
+                                    }
+                                })
+                                if (count_fio != 0) {
+                                    const obj = {
+                                        //title: workerlist[i-1].spec,
+                                        title2: arritem,
+                                        count_fio: count_fio,
+                                        count_title: count_title,
+                                    }
+                                    arr_count.push(obj)
+                                } else if (count_title !=0) {
+                                    const obj = {
+                                        //title: Worklist[i-1].spec,
+                                        title2: arritem,
+                                        count_fio: count_fio,
+                                        count_title: count_title,
+                                    }
+                                    arr_count.push(obj) 
+                                }
+                            }  
+                        })
+
+                        //сохранение массива в 2-х элементный массив
+                        if (i % 2 == 0) {
+                            arr_all[0] = arr_count
+                        } else {
+                            arr_all[1] = arr_count 
+                        }
+
+                        var isEqual = JSON.stringify(arr_all[0]) === JSON.stringify(arr_all[1]);
+                        // если есть изменения в составе работников    
+                        if (!isEqual) {
+                            //отправка сообщения в чат бота
+                            await bot.sendMessage(chatId, 
+                                `Запрос на специалистов: 
+                                                                    
+${projectDate} | ${projectTime} | ${projectName} | U.L.E.Y
+
+${arr_count.map((item, index) =>'0' + (index+1) + '. '+ item.title + ' = ' + item.count_fio + '\/' + item.count_title + ' [' + item.title2 + ']'
+).join('\n')}`                    
+                            )
+                        } else {
+                            
+                        }
+
+                    }, 60000); //каждую минуту 
+
+
+                    // остановить вывод через 260 минут
+                    setTimeout(() => { clearInterval(timerId); }, 15600000); //260 минут
+
+                } catch (error) {
+                    console.log("Ошибка: ", error) 
+                }
+
             } else if (text.includes('Проект успешно создан')) {           
                 await bot.sendMessage(chatTelegramId, `${text} \n \n от ${firstname} ${lastname} ${chatId}`)
                 await bot.sendMessage(chatGiaId, `${text} \n \n от ${firstname} ${lastname} ${chatId}`)
