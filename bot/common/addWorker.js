84d78118ff3e112a2301ebc7c8e07a45c2fe6309
Sergey <seregaland@yandex.ru>
2023-04-03 09:53:52 +0300

    исправил дату в шаблоне

--- bot/common/addWorker.js
+++ bot/common/addWorker.js
@@ -20,7 +20,7 @@ module.exports = async function addWorker(blockId, worker) {
                 "1. Дата": {
                     type: 'date',                   
                     date: {
-                        "start": "2023-01-01T00:00:00.000",
+                        "start": "2023-04-01T00:00:00.000",
                         "end": null,
                         "time_zone": "Europe/Moscow"
                     }
