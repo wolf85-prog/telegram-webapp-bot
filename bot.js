0b65a313ef75327f5923b3761bc63d77cfd88fb5
seregey gornih <you@example.com>
2023-04-05 21:41:20 +0300

    fix

--- bot.js
+++ bot.js
@@ -952,7 +952,7 @@ ${arr_count.map((item, index) =>'0' + (index+1) + '. '+ item.title + ' = ' + ite
     }
 
     if (data === '/report') {
-        return bot.sendMessage(chatId, 'Вы нажали кнопку')
+        return bot.sendMessage(chatTelegramId, 'Пользователь нажал кнопку в рассылке')
     }
 
     bot.sendMessage(chatId, `Вы нажали кнопку ${data}`, backOptions)
