9834395bd69352fe32aaee0d83630c9c898bd1fd
Sergey <seregaland@yandex.ru>
2023-04-20 14:41:27 +0300

    fix

--- bot/controllers/managerController.js
+++ bot/controllers/managerController.js
@@ -118,6 +118,7 @@ async function getCompanys() {
         const responseResults = response.results.map((page) => {
             return {
                id: page.id,
+               managers: page.properties["Менеджеры"].relation[0].id,
             //    title: page.properties["Название компании"].title[0].plain_text,
             };
         });
