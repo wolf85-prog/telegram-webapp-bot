56e097ad1124ffd3beb308e54dc8ccdd5fac33eb
Sergey <seregaland@yandex.ru>
2023-06-03 16:30:42 +0300

    fix

--- bot.js
+++ bot.js
@@ -852,6 +852,7 @@ bot.on('message', async (msg) => {
                 let str_text
                 if (msg.reply_to_message) {
                     const message = await Message.findOne({where:{messageId: msg.reply_to_message.message_id.toString()}}) 
+                    console.log(message)
                    str_text = `${message}_reply_${text}`                 
                 } else {
                     str_text = text
