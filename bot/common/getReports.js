5e86a79ffc612bb26b21e47b867f7df5272f10ae
Sergey <seregaland@yandex.ru>
2023-06-14 14:48:18 +0300

    test

--- bot/common/getReports.js
+++ bot/common/getReports.js
@@ -137,6 +137,7 @@ module.exports = async function getReports(project, bot) {
 
         //сравнить два массива и узнать есть ли изменения
         let isEqual = JSON.stringify(arr_all[0]) === JSON.stringify(arr_all[1]);
+        console.log(arr_count)
 
         if (!isEqual) {
 
