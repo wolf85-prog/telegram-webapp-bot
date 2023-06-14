109e377afbbdfe5acb685c86b1a56aa91e6c89b9
Sergey <seregaland@yandex.ru>
2023-06-14 15:12:39 +0300

    fix

--- bot/controllers/databaseController.js
+++ bot/controllers/databaseController.js
@@ -29,15 +29,17 @@ async function getDatabaseId(baseId) {
             database_id: baseId
         });
 
-        // const responseResults = response.results.map((page) => {
-        //     if ()
-        //     return {
-        //         date: page.properties["1. Дата"].date?.start,
-        //         fio: page.properties["2. ФИО"].relation[0]?.id,
-        //         title: page.properties["3. Специализация"].multi_select[0]?.name, 
-        //         spec: page.properties["3. Специализация"].multi_select[1]?.name              
-        //     };
-        // });
+        const responseResults = response.results.map((page) => {
+            if (page.properties["1. Дата"].date) {
+                return {
+                    date: page.properties["1. Дата"].date?.start,
+                    fio: page.properties["2. ФИО"].relation[0]?.id,
+                    title: page.properties["3. Специализация"].multi_select[0]?.name, 
+                    spec: page.properties["3. Специализация"].multi_select[1]?.name              
+                };
+            }
+            
+        });
 
         return response.results;
     } catch (error) {
