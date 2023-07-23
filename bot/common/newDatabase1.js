e490f97c7311dd608da7602dc6f2cd731c7d55b4
seregey gornih <you@example.com>
2023-07-23 22:49:42 +0300

    fix

--- bot/common/newDatabase1.js
+++ bot/common/newDatabase1.js
@@ -46,15 +46,6 @@ module.exports = async function newDatabase1(parent_page_id) {
                                 "name": "День №4",
                                 "color": "blue"
                             },
-                        ]
-                    }
-                },
-                "Дата №1": {
-                    "date": {}
-                },
-                "График №2": {
-                    "multi_select": {
-                        "options": [
                             {
                                 "name": "День №5",
                                 "color": "orange"
@@ -70,6 +61,175 @@ module.exports = async function newDatabase1(parent_page_id) {
                             {
                                 "name": "День №8",
                                 "color": "orange"
+                            },
+                            {
+                                "name": "День №9",
+                                "color": "green"
+                            },
+                            {
+                                "name": "День №10",
+                                "color": "green"
+                            },
+                            {
+                                "name": "День №11",
+                                "color": "green"
+                            },
+                            {
+                                "name": "День №12",
+                                "color": "green"
+                            },
+                            {
+                                "name": "День №13",
+                                "color": "purple"
+                            },
+                            {
+                                "name": "День №14",
+                                "color": "purple"
+                            },
+                            {
+                                "name": "День №15",
+                                "color": "purple"
+                            },
+                            {
+                                "name": "День №16",
+                                "color": "purple"
+                            },
+                            {
+                                "name": "День №17",
+                                "color": "yellow"
+                            },
+                            {
+                                "name": "День №18",
+                                "color": "yellow"
+                            },
+                            {
+                                "name": "День №19",
+                                "color": "yellow"
+                            },
+                            {
+                                "name": "День №20",
+                                "color": "yellow"
+                            },
+                            {
+                                "name": "День №21",
+                                "color": "pink"
+                            },
+                            {
+                                "name": "День №22",
+                                "color": "pink"
+                            },
+                            {
+                                "name": "День №23",
+                                "color": "pink"
+                            },
+                            {
+                                "name": "День №24",
+                                "color": "pink"
+                            },
+                        ]
+                    }
+                },
+                "Дата №1": {
+                    "date": {}
+                },
+                "График №2": {
+                    "multi_select": {
+                        "options": [
+                            {
+                                "name": "День №1",
+                                "color": "blue"
+                            },
+                            {
+                                "name": "День №2",
+                                "color": "blue"
+                            },
+                            {
+                                "name": "День №3",
+                                "color": "blue"
+                            },
+                            {
+                                "name": "День №4",
+                                "color": "blue"
+                            },
+                            {
+                                "name": "День №5",
+                                "color": "orange"
+                            },
+                            {
+                                "name": "День №6",
+                                "color": "orange"
+                            },
+                            {
+                                "name": "День №7",
+                                "color": "orange"
+                            },
+                            {
+                                "name": "День №8",
+                                "color": "orange"
+                            },
+                            {
+                                "name": "День №9",
+                                "color": "green"
+                            },
+                            {
+                                "name": "День №10",
+                                "color": "green"
+                            },
+                            {
+                                "name": "День №11",
+                                "color": "green"
+                            },
+                            {
+                                "name": "День №12",
+                                "color": "green"
+                            },
+                            {
+                                "name": "День №13",
+                                "color": "purple"
+                            },
+                            {
+                                "name": "День №14",
+                                "color": "purple"
+                            },
+                            {
+                                "name": "День №15",
+                                "color": "purple"
+                            },
+                            {
+                                "name": "День №16",
+                                "color": "purple"
+                            },
+                            {
+                                "name": "День №17",
+                                "color": "yellow"
+                            },
+                            {
+                                "name": "День №18",
+                                "color": "yellow"
+                            },
+                            {
+                                "name": "День №19",
+                                "color": "yellow"
+                            },
+                            {
+                                "name": "День №20",
+                                "color": "yellow"
+                            },
+                            {
+                                "name": "День №21",
+                                "color": "pink"
+                            },
+                            {
+                                "name": "День №22",
+                                "color": "pink"
+                            },
+                            {
+                                "name": "День №23",
+                                "color": "pink"
+                            },
+                            {
+                                "name": "День №24",
+                                "color": "pink"
                             },
                         ]
                     }
