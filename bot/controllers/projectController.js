1ef220af000a9bacd61d1be5f6baf4b62adfad19
seregey gornih <you@example.com>
2023-07-23 22:22:28 +0300

    fix get projects

--- bot/controllers/projectController.js
+++ bot/controllers/projectController.js
@@ -13,14 +13,14 @@ async function getProjects() {
             return {
                id: page.id,
                title: page.properties.Name.title[0]?.plain_text,
-               time: page.properties.Date.date,
-               time_start: page.properties.Date.date.start,
+               time: page.properties["Дата"].date,
+               time_start: page.properties["Дата"].date.start,
                time_created: page.created_time,
-               geo: '',//page.properties.Address.rollup.array,
-               teh: page.properties.TechClient.rich_text,
-               status_id: page.properties.Status.select,
-               manager: page.properties.Manager.relation[0]?.id,
-               company: page.properties.Company.relation[0]?.id,
+               geo: '', //page.properties.Address.rollup.array,
+               teh: page.properties["Тех. задание"].rich_text,
+               status_id: page.properties["Статус проекта"].select,
+               manager: page.properties["Менеджер"].relation[0]?.id,
+               company: page.properties["Компания"].relation[0]?.id,
                worklist:'',
             };
         });
@@ -64,7 +64,7 @@ async function getProjectsId(managerId) {
             "filter": {
                 "and": [
                     {
-                        "property": "Manager",
+                        "property": "Менеджер",
                         "relation": {
                             "contains": managerId
                         },
@@ -94,14 +94,14 @@ async function getProjectsId(managerId) {
             return {
                id: page.id,
                title: page.properties.Name.title[0]?.plain_text,
-               time: page.properties.Date.date,
-               time_start: page.properties.Date.date.start,
+               time: page.properties["Дата"].date,
+               time_start: page.properties["Дата"].date.start,
                time_created: page.created_time,
                geo: '', //page.properties.Address.rollup.array,
-               teh: page.properties.TechClient.rich_text,
-               status_id: page.properties.Status.select,
-               manager: page.properties.Manager.relation[0]?.id,
-               company: page.properties.Company.relation[0]?.id,
+               teh: page.properties["Тех. задание"].rich_text,
+               status_id: page.properties["Статус проекта"].select,
+               manager: page.properties["Менеджер"].relation[0]?.id,
+               company: page.properties["Компания"].relation[0]?.id,
                worklist:'',
             };
         });
