399cc56fe4aac2f6319adf8cea5dc4cf0de83b7a
seregey gornih <you@example.com>
2023-08-23 15:39:08 +0300

    fix

--- bot/common/getProjectNew.js
+++ bot/common/getProjectNew.js
@@ -16,30 +16,30 @@ module.exports = async function getProjectNew() {
             if (blockId) { 
                 databaseBlock = await getDatabaseId(blockId);  
                 //console.log(databaseBlock)
-                // if (databaseBlock && databaseBlock?.length !== 0) {
-                //     let project = databaseBlock.find(item => new Date(item.date) >= d)
-                //     // return {
-                //     //     id: page.id,
-                //     //     name: page.name,
-                //     //     datestart: project.date,
-                //     // }
-                //     //arr.push(obj)
-                //     console.log(project)
-                // }
+                if (databaseBlock && databaseBlock?.length !== 0) {
+                    //let project = databaseBlock.find(item => new Date(item.date) >= d)
+                    // return {
+                    //     id: page.id,
+                    //     name: page.name,
+                    //     datestart: project.date,
+                    // }
+                    //arr.push(obj)
+                    console.log("проект")
+                }
 
-                databaseBlock.map(item => {
-                    if (new Date(item.date) >= d) {
-                        const obj = {
-                            id: page.id,
-                            name: page.name,
-                            date: item.date,
-                        }
-                        arr.push(obj) 
-                    }
-                })
+                // databaseBlock.map(item => {
+                //     if (new Date(item.date) >= d) {
+                //         const obj = {
+                //             id: page.id,
+                //             name: page.name,
+                //             date: item.date,
+                //         }
+                //         arr.push(obj) 
+                //     }
+                // })
                 
             }
-            console.log("проект")
+            //console.log("проект")
             
         })
 
