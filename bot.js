2edfe9bd4bf89fd653516c333a7d3242536d68ea
seregey gornih <you@example.com>
2023-09-11 21:10:16 +0300

    fix smeta

--- bot.js
+++ bot.js
@@ -1007,32 +1007,31 @@ bot.on('message', async (msg) => {
         console.log("block2: ", block2.results[0].id)
                         
         const block3 = await getBlock(block2.results[0].id)
-        console.log("block3: ", block3.results[0].id)
-             
+        console.log("block3: ", block3.results[0].id)            
         
         //поставить галочку в проекте в поле Предварительная смета
         await updateToDo(block3.results[0].id);
 
         const poster = `${host}/files/${crmId}/pre/${crmId}_${chatId}_customer_1.pdf`
         console.log("poster: ", poster)
-
+        
         //найти смету по свойству Проект
-        //const smetaId = await getSmeta(projectId[1])
+        const smetaId = await getSmeta(projectId[1])
 
-        //изменить тег в таб. Сметы в поле Финал. смета на Подтверждена
-        //await updateSmeta(smetaId)
+        //изменить тег в таб. Сметы в поле смета на Подтверждена
+        await updateSmeta(smetaId)
 
-        //начать цикл сканирования отметки Смета
-        // 86400 секунд в дне
-        var minutCount = 0;
-            
+        //начать цикл сканирования отметки Смета     
         // повторить с интервалом 2 минуты
         // let timerId = setInterval(async() => {
-            
-        // }, 120000); //каждые 2 минуты
 
-        // остановить вывод через 30 дней
-        // if (minutCount == 43200) {
+        //     //изменить тег в таб. Сметы в поле смета на Подтверждена
+        //     await updateSmeta(smetaId)
+            
+        // }, 60000); //каждую 1 минуту
+
+        // // остановить вывод если проект недоступен
+        // if (!block1) {
         //     clearInterval(timerId);
         // }
 
@@ -1079,10 +1078,10 @@ bot.on('message', async (msg) => {
         console.log("poster: ", poster)
 
         //найти смету по свойству Проект
-        //const smetaId = await getSmeta(projectId[1])
+        const smetaId = await getSmeta(projectId[1])
 
         //изменить тег в таб. Сметы в поле Финал. смета на Подтверждена
-        //await updateSmetaFinal(smetaId)
+        await updateSmetaFinal(smetaId)
 
 
         // Подключаемся к серверу socket
