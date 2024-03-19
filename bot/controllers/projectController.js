057b87c11943ee910675f7d6936974102f17e40c
seregey gornih <you@example.com>
2024-03-19 11:02:28 +0300

    fix

--- bot/controllers/projectController.js
+++ bot/controllers/projectController.js
@@ -4,6 +4,7 @@ const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_DATABASE_ID
 const {Projectcash} = require('../models/models')
 const getDatabaseId = require("./../common/getDatabaseId");
+const getBlocks = require("./../common/getBlocks");
 
 async function getProjects() {
     try {
