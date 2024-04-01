341bf90cf52da90729b58bd11a7c15860883a410
seregey gornih <you@example.com>
2024-04-01 17:54:18 +0300

    fix

--- bot/common/getDatabaseId.js
+++ bot/common/getDatabaseId.js
@@ -14,7 +14,7 @@ module.exports = async function getDatabaseId(baseId) {
                 id: page.id,        
                 date: page.properties["02. Дата"] ? page.properties["02. Дата"].date?.start : page.properties["2. Дата"].date?.start,
                 fio: page.properties["04. ФИО"] ? page.properties["04. ФИО"].relation[0]?.id : page.properties["4. ФИО"].relation[0]?.id,
-                vid: page.properties["03. Вид работ"] ? page.properties["03. Вид работ"].multi_select[0]?.name : page.properties["3.  Вид работ"].multi_select[0]?.name,
+                vid: page.properties["03. Вид работ"] ? page.properties["03. Вид работ"].multi_select[0]?.name : page.properties["3. Вид работ"].multi_select[0]?.name,
                 spec: page.properties["05. Специализация"] ? page.properties["05. Специализация"].multi_select[0]?.name : page.properties["5. Специализация"].multi_select[0]?.name
             };
         });
