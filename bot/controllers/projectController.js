37282d8df3c5b65aad00bdcb96fb96b59e9491ab
Sergey Gornih <seregaland@yandex.ru>
2024-09-16 12:25:08 +0300

    fix get projects

--- bot/controllers/projectController.js
+++ bot/controllers/projectController.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Client } = require("@notionhq/client");
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_DATABASE_ID
-const {Projectcash, ProjectNew} = require('../models/models')
+const {Projectcash, ProjectNew, Project} = require('../models/models')
 const getDatabaseId = require("./../common/getDatabaseId");
 const getBlocks = require("./../common/getBlocks");
 
@@ -74,56 +74,63 @@ async function getProjects3() {
 async function getProjectsId(managerId) {
     //console.log("managerId: ", managerId)
     try {
-        const response = await notion.databases.query({
-            database_id: databaseId,
-            "filter": {
-                "and": [
-                    {
-                        "property": "Менеджер",
-                        "relation": {
-                            "contains": managerId
-                        },
-                    },
-                    {
-                        // "property": "Date",
-                        // "date": {
-                        //     "after": "2023-05-31"
-                        // }
-                        "timestamp": "created_time",
-                        "created_time": {
-                            "after": "2023-05-31"
-                        }
-                    }
-                ]
+        // const response = await notion.databases.query({
+        //     database_id: databaseId,
+        //     "filter": {
+        //         "and": [
+        //             {
+        //                 "property": "Менеджер",
+        //                 "relation": {
+        //                     "contains": managerId
+        //                 },
+        //             },
+        //             {
+        //                 // "property": "Date",
+        //                 // "date": {
+        //                 //     "after": "2023-05-31"
+        //                 // }
+        //                 "timestamp": "created_time",
+        //                 "created_time": {
+        //                     "after": "2023-05-31"
+        //                 }
+        //             }
+        //         ]
                 
-            },
-            "sorts": [{ 
-                "timestamp": "created_time", 
-                "direction": "descending" 
-            }]
-        });
+        //     },
+        //     "sorts": [{ 
+        //         "timestamp": "created_time", 
+        //         "direction": "descending" 
+        //     }]
+        // });
 
-        //return response.results[0].id;
+        // const responseResults = response.results.map((page) => {
+        //     return {
+        //        id: page.id,
+        //        title: page.properties.Name.title[0]?.plain_text,
+        //        time: page.properties["Дата"].date,
+        //        time_start: page.properties["Дата"].date?.start,
+        //        time_created: page.created_time,
+        //        geo: '', //page.properties.Address.rollup.array,
+        //        teh: page.properties["Тех. задание"].rich_text,
+        //        status_id: page.properties["Статус проекта"].select,
+        //        manager: page.properties["Менеджер"].relation[0]?.id,
+        //        company: page.properties["Компания"].relation[0]?.id,
+        //        worklist:'',
+        //        crmID: page.properties.Crm_ID.rich_text[0]?.plain_text,
+        //     };
+        // });
 
-        const responseResults = response.results.map((page) => {
-            return {
-               id: page.id,
-               title: page.properties.Name.title[0]?.plain_text,
-               time: page.properties["Дата"].date,
-               time_start: page.properties["Дата"].date?.start,
-               time_created: page.created_time,
-               geo: '', //page.properties.Address.rollup.array,
-               teh: page.properties["Тех. задание"].rich_text,
-               status_id: page.properties["Статус проекта"].select,
-               manager: page.properties["Менеджер"].relation[0]?.id,
-               company: page.properties["Компания"].relation[0]?.id,
-               worklist:'',
-               crmID: page.properties.Crm_ID.rich_text[0]?.plain_text,
-            };
-        });
+        const response = await Project.findAll({
+            order: [
+                ['id', 'DESC'],
+            ],
+            where: {
+                chatId: managerId.toString()
+            } 
+        })
+        
 
-        //console.log("Projects Data: "  + JSON.stringify(responseResults))
-        return responseResults;
+        return response;
     } catch (error) {
         console.error(error.message)
     }
